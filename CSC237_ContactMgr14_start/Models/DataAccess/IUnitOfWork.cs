033d4f1d7cf3609a58caa555b01718b352af051d
Kathleen Geise <kmgeise2@gmail.com>
2022-04-13 18:28:41 -0600

    Add project files.

---
+++ CSC237_ContactMgr14_start/Models/DataAccess/IUnitOfWork.cs
@@ -0,0 +1,11 @@
+﻿
+namespace CSC237_ContactMgr14_start.Models
+{
+    public interface IUnitOfWork
+    {
+        IRepository<Contact> Contacts { get; }
+        IRepository<Category> Categories { get; }
+
+        void Save();
+    }
+}
